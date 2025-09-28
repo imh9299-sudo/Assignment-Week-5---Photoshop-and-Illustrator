9b0661e3cfbbd71fc3a9dc4c3945f004a271ff29
Isabella Hernandez <imh9299@nyu.edu>
2025-09-28 10:23:41 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+let blueShape=document.getElementById("Blue")
+ 
+
+blueShape.addEventListener("click", () => {
+   document.getElementById("textbox").innerHTML =  '<img src="screenshot.png" alt="Good morning" style="width:300px;">';
+});

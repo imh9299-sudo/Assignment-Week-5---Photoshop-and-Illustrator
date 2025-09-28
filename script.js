c74a537665251ed662de517ebc017028107d523f
Isabella Hernandez <imh9299@nyu.edu>
2025-09-28 10:25:47 +0400

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-let blueShape=document.getElementById("Blue")
- 
-
-blueShape.addEventListener("click", () => {
-   document.getElementById("textbox").innerHTML =  '<img src="screenshot.png" alt="Good morning" style="width:300px;">';
-});
